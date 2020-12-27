d303cbfb613e1c976f9d8dc65dc4a0fcb6b2f8f1
Ajala John <treformers@gmail.com>
2020-12-26 22:07:41 -0600

    Created Data Classes and DTOs, Also included automapper and change appsettings file.

---
+++ Mappings/Maps.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BookStore_API.Data;
+using BookStore_API.DTOs;
+
+namespace BookStore_API.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<Author, AuthorDTO>().ReverseMap();
+            //CreateMap<Author, AuthorCreateDTO>().ReverseMap();
+            //CreateMap<Author, AuthorUpdateDTO>().ReverseMap();
+            CreateMap<Book, BookDTO>().ReverseMap();
+            //CreateMap<Book, BookCreateDTO>().ReverseMap();
+            //CreateMap<Book, BookUpdateDTO>().ReverseMap();
+        }
+    }
+}
