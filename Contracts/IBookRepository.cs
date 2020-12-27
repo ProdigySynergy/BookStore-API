6b6c2e2b9e3993970cc2f20e2bd3dd28f58f1a38
Ajala John <treformers@gmail.com>
2020-12-27 08:40:30 -0600

    Implemented Author API Endpoints and required functionality.

---
+++ Contracts/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookStore_API.Data;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    public interface IBookRepository : IRepositoryBase<Book>
+    {
+        public Task<string> GetImageFileName(int id);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -75,6 +75,8 @@ namespace BookStore_API
             });
 
             services.AddSingleton<ILoggerService, LoggerService>();
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
 
             services.AddControllers();
         }
